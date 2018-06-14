27c58c811e718dea30dc2a66bd7985342b5a903e
CKPRATHEESH <ckpratheesh.prema@gmail.com>
2018-06-14 19:22:08 +0530

    More

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ContractsModule } from './contracts/contracts.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContractsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
